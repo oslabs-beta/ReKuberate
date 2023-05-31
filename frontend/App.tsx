dca11fedbf40af3a1cbd8ed05145b37f85b593e1
Fabrizzio Quintanilla <fabrizzioquintanilla@Fabrizzios-MBP.home>
2023-05-31 12:10:12 -0400

    added the sidebarNav, added pods page with mockdata

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-
 import { Route, Routes } from 'react-router-dom';
 import HomeContainer from './containers/HomeContainer';
+import PodsContainer from './containers/PodsContainer';
 import SidebarContainer from './containers/SidebarContainer';
+import styles from './stylesheets/styles.module.scss'
 
 export default function App() {
   return (
     <>
       <SidebarContainer />
-      <Routes>
-        <Route path='/' element={<HomeContainer />}></Route>
-      </Routes>
+      <div className={styles.mainDiv}>
+        <Routes>
+          <Route path='/' element={<HomeContainer />}></Route>
+          <Route path='/pods' element={<PodsContainer />}></Route>
+        </Routes>
+      </div>      
     </>
   );
 }
