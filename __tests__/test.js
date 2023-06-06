d475beee1c9e34196d2a2f07501307ba6954d317
thadd225 <thadd225@gmail.com>
2023-06-05 20:21:08 -0400

    Added testing logic for getting pod data
    Co-authored-by: Fab3005 <Fab3005@users.noreply.github.com>
    Co-authored-by: farrellkai <farrellkai@users.noreply.github.com>
    Co-authored-by: Hunter Shaw <HShaw215@users.noreply.github.com>
    Co-authored-by: Kevin Fan <kevinlifan@yahoo.com>

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,28 +1,44 @@
 import { exec, spawn, spawnSync } from 'child_process';
 import puppeteer from 'puppeteer';
 
-console.log('minikube start')
-let bat = spawnSync('minikube start', { shell: true , encoding: 'utf-8'})                                                       ;
-console.log(bat.stdout)
+// console.log('minikube start')
+// let bat = spawnSync('minikube start', { shell: true , encoding: 'utf-8'})                                                       ;
+// console.log(bat.stdout)
 
-console.log('helm repo add prometheus-community https://prometheus-community.github.io/helm-charts')
-bat = spawnSync('helm repo add prometheus-community https://prometheus-community.github.io/helm-charts', { shell: true , encoding: 'utf-8'})                                                       ;
-console.log(bat.stdout)
+console.log(
+  'helm repo add prometheus-community https://prometheus-community.github.io/helm-charts'
+);
+let bat = spawnSync(
+  'helm repo add prometheus-community https://prometheus-community.github.io/helm-charts',
+  { shell: true, encoding: 'utf-8' }
+);
+console.log(bat.stdout);
 
-console.log('helm repo add stable https://charts.helm.sh/stable')
-bat = spawnSync('helm repo add stable https://charts.helm.sh/stable', { shell: true , encoding: 'utf-8'})                                                       ;
-console.log(bat.stdout)
+console.log('helm repo add stable https://charts.helm.sh/stable');
+bat = spawnSync('helm repo add stable https://charts.helm.sh/stable', {
+  shell: true,
+  encoding: 'utf-8',
+});
+console.log(bat.stdout);
 
-console.log('helm repo update')
-bat = spawnSync('helm repo update', { shell: true , encoding: 'utf-8'})                                                       ;
-console.log(bat.stdout)
+console.log('helm repo update');
+bat = spawnSync('helm repo update', { shell: true, encoding: 'utf-8' });
+console.log(bat.stdout);
 
-console.log('helm install prometheus prometheus-community/kube-prometheus-stack')
-bat = spawnSync('helm install prometheus prometheus-community/kube-prometheus-stack', { shell: true , encoding: 'utf-8'})                                                       ;
-console.log(bat.stdout)
+console.log(
+  'helm install prometheus prometheus-community/kube-prometheus-stack'
+);
+bat = spawnSync(
+  'helm install prometheus prometheus-community/kube-prometheus-stack',
+  { shell: true, encoding: 'utf-8' }
+);
+console.log(bat.stdout);
 
-console.log('kubectl port-forward deployment/prometheus-grafana 3000')
-bat = spawn('kubectl port-forward deployment/prometheus-grafana 3000', { shell: true, detached: true })                                                       ;
+console.log('kubectl port-forward deployment/prometheus-grafana 3000');
+bat = spawn('kubectl port-forward deployment/prometheus-grafana 3000', {
+  shell: true,
+  detached: true,
+});
 bat.stdout.on('data', (data) => {
   console.log(data.toString());
 });
@@ -31,11 +47,10 @@ bat.on('exit', (code) => {
   console.log(`Child exited with code ${code}`);
 });
 
-const test = async () => {
-  const browser = await puppeteer.launch({headless: false});
-  const page = await browser.newPage();
+// const test = async () => {
+//   const browser = await puppeteer.launch({ headless: false });
+//   const page = await browser.newPage();
 
-  await page.goto('http://localhost:3000');
-
-};
-test()
+//   await page.goto('http://localhost:3000');
+// };
+// test();
