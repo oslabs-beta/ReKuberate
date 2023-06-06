9601ee4430c6d6f6b5ac814b057c86fabd8e2270
thadd225 <thadd225@gmail.com>
2023-06-06 11:58:11 -0400

    created clusterController functionality for getting pod and node info, added types for obj

--- server/types.ts
+++ server/types.ts
@@ -1,17 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
 
-export type middleware = (req: Request, res: Response, next: NextFunction) => void
+export type middleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => void;
 
 //Declare controller types
 export type InitControllerType = {
-  installPrometheus: middleware,
-  installGrafana: middleware
-}
+  installPrometheus: middleware;
+  installGrafana: middleware;
+};
 
 export type ClusterControllerType = {
-  getPodInfo : middleware,
-  getNodeInfo: middleware
-}
+  getPodAndNodeInfo: middleware;
+  // getNodeInfo: middleware
+};
 
 //Declare ErrorHandler type and assign log to string, status to number, and message to string
 export type ErrorHandler = {
@@ -21,3 +25,11 @@ export type ErrorHandler = {
     err: string;
   };
 };
+
+export type obj = {
+  [key: string]: {
+    [key: string]: any;
+  };
+};
+
+// export type podsSplit = string | string[];
