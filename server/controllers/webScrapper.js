5e6b72438362a219becc439616727b03ee7b2b73
Kevin Fan <kevinlifan@yahoo.com>
2023-06-08 15:36:36 -0400

    headless to true

--- server/controllers/webScrapper.js
+++ server/controllers/webScrapper.js
@@ -8,7 +8,7 @@ function delay(time) {
 }
 
 async function webScrapper() {
-  const browser = await puppeteer.launch({ headless: false, args: [`--window-size=1900,1000`] });
+  const browser = await puppeteer.launch({ headless: true, args: [`--window-size=1900,1000`] });
   const page = await browser.newPage();
   //set puppeteer chrome window screensize for all users
   await page.setViewport({ width: 1900, height: 1000 });
