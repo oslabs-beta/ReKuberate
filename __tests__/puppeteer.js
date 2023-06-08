03accd0ae7815b417669254726e7c1430f0cf893
Kevin Fan <kevinlifan@yahoo.com>
2023-06-08 12:31:28 -0400

    some stuff

--- __tests__/puppeteer.js
+++ __tests__/puppeteer.js
@@ -75,44 +75,20 @@ async function installMouseHelper(page) {
 };
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false, args: ['--start-maximized'] });
+  const browser = await puppeteer.launch({ headless: false, args: ['--window-size=1900,1000'] });
   const page = await browser.newPage();
+  //set puppeteer chrome window screensize for all users
   await page.setViewport({ width: 1900, height: 1000});
   await installMouseHelper(page);
-  await page.goto('http://localhost:9000/');
-  const signIn = '#reactRoot > div.grafana-app > main > div.css-278jzv > div.css-13883cc > div.css-68hv8y > a';
 
-  await page.waitForSelector(signIn);
-  await page.click(signIn);
-
-  const usernameInput =
-    '#reactRoot > div.grafana-app > main > div > div.css-opq959 > div > div.css-9h8xxw > div > div > form > div:nth-child(1) > div:nth-child(2) > div > div > input';
-
-  await page.waitForSelector(usernameInput);
-  await page.type(usernameInput, 'admin');
-  const passwordInput = '#current-password';
-
-  await page.waitForSelector(passwordInput);
-  await page.type(passwordInput, 'prom-operator');
-  const loginButton =
-    '#reactRoot > div.grafana-app > main > div > div.css-opq959 > div > div.css-9h8xxw > div > div > form > button';
-
-  await page.waitForSelector(loginButton);
-  await page.click(loginButton);
-  await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
   await page.goto('http://localhost:9000/dashboards');
-  const generalFolder =
-    '#reactRoot > div.grafana-app > main > div.css-60onds > div.css-1syiu8h-page-wrapper > div > div.css-1rs2yug-page-container > div > div.scrollbar-view > div > div.css-1736fpx-page-content > div > div.css-1rf724w > div > div';
-
-  await page.waitForSelector(generalFolder);
-  await page.click(generalFolder);
 
   const searchInput =
     '#reactRoot > div.grafana-app > main > div.css-60onds > div.css-1syiu8h-page-wrapper > div > div.css-1rs2yug-page-container > div > div.scrollbar-view > div > div.css-1736fpx-page-content > div > div.css-vbrrr8.page-action-bar > div > div > div > input';
   await page.waitForSelector(searchInput);
   await page.type(searchInput, 'kubelet');
 
-  await delay(1000);
+  await delay(500);
 
   const kublet =
     '#reactRoot > div.grafana-app > main > div.css-60onds > div.css-1syiu8h-page-wrapper > div > div.css-1rs2yug-page-container > div > div.scrollbar-view > div > div.css-1736fpx-page-content > div > div:nth-child(3) > div:nth-child(1) > div > div:nth-child(2) > div > div > div > a';
@@ -120,17 +96,105 @@ async function installMouseHelper(page) {
   await page.waitForSelector(kublet);
   await page.click(kublet);
 
-  await delay(1000);
+  await delay(500);
 
-  const dotDotDotButton =
-    '/html/body/div[1]/div[1]/main/div[2]/div[3]/div/div[1]/div/div/div[1]/div/div/div[1]/div/div/div[2]/button/div/svg';
-
+  //opens menu and clicks on embed to get link for kublets
   await page.mouse.click(310, 160)
   await delay(500);
   await page.mouse.click(300, 235)
   await delay(500);
-  await page.mouse.click(900, 140)
-  const textSelector = await page.waitForSelector('#share-panel-embed-embed-html-textarea')
-  const url = await textSelector?.evaluate(el => el.textContent);
-  console.log(url)
+  await page.mouse.click(820, 135)
+  let textSelector = await page.waitForSelector('#share-panel-embed-embed-html-textarea')
+  const numOfKublets = await textSelector?.evaluate(el => el.textContent);
+  console.log(numOfKublets)
+  //exits out of menu
+  await delay(500);
+  await page.mouse.click(1290, 135)
+
+  await delay(500);
+
+  //open menu for number of pods and gets embed
+  await page.mouse.click(610, 160)
+  await delay(500);
+  await page.mouse.click(600, 235)
+  await delay(500);
+  await page.mouse.click(820, 135)
+  textSelector = await page.waitForSelector('#share-panel-embed-embed-html-textarea')
+  const numOfPods = await textSelector?.evaluate(el => el.textContent);
+  console.log(numOfPods)
+  await delay(500);
+  await page.mouse.click(1290, 135)
+  
+  await delay(500);
+
+  //open menu for number of containers and gets embed
+  await page.mouse.click(930, 160)
+  await delay(500);
+  await page.mouse.click(900, 235)
+  await delay(500);
+  await page.mouse.click(820, 135)
+  textSelector = await page.waitForSelector('#share-panel-embed-embed-html-textarea')
+  const numOfContainers = await textSelector?.evaluate(el => el.textContent);
+  console.log(numOfContainers)
+  await delay(500);
+  await page.mouse.click(1290, 135)
+  
+  await delay(500);
+
+  //click on dashboard to go back out to all dashboards
+  await page.goto('http://localhost:9000/dashboards');
+
+  await delay(500)
+
+  const searchInput1 =
+    '#reactRoot > div.grafana-app > main > div.css-60onds > div.css-1syiu8h-page-wrapper > div > div.css-1rs2yug-page-container > div > div.scrollbar-view > div > div.css-1736fpx-page-content > div > div.css-vbrrr8.page-action-bar > div > div > div > input';
+  await page.waitForSelector(searchInput1);
+  await page.type(searchInput1, 'Node Exporter / Nodes');
+
+  await delay(500);
+  const selector = '#reactRoot > div.grafana-app > main > div.css-60onds > div.css-1syiu8h-page-wrapper > div > div.css-1rs2yug-page-container > div > div.scrollbar-view > div > div.css-1736fpx-page-content > div > div:nth-child(3) > div:nth-child(1) > div > div:nth-child(2) > div > div > div > a';
+
+  await page.waitForSelector(selector);
+  await page.click(selector);
+
+  await delay(500);
+  
+  //opens share menu for cpu usage and gets embed
+  await page.mouse.click(933, 193)
+  await delay(500);
+  await page.mouse.click(815, 266)
+  await delay(500);
+  await page.mouse.click(831, 131);
+  await delay(500);
+  textSelector = await page.waitForSelector('#share-panel-embed-embed-html-textarea')
+  const url4 = await textSelector?.evaluate(el => el.textContent);
+  console.log(url4)
+  await delay(500);
+  await page.mouse.click(1290, 135)
+  await delay(500);
+  
+  //click on memory for memory usage (graph)
+  await page.mouse.click(1402, 495);
+  await delay(500)
+  await page.mouse.click(1295, 568)
+  await delay(500)
+  await page.mouse.click(834, 132)
+  await delay(500)
+  textSelector = await page.waitForSelector('#share-panel-embed-embed-html-textarea');
+  const url5 = await textSelector?.evaluate(el => el.textContent);
+  console.log(url5);
+  await delay(500);
+  await page.mouse.click(1290, 135)
+  await delay(500);
+  
+  //click on menu for memory usage (dial)
+  await page.mouse.click(1700, 495);
+  await delay(500)
+  await page.mouse.click(1595, 568)
+  await delay(500)
+  await page.mouse.click(1134, 132)
+  await delay(500)
 })();
+
+export default puppeteer;
+
--- server/controllers/initController.ts
+++ server/controllers/initController.ts
@@ -4,12 +4,12 @@ import { InitControllerType } from '../types';
 const initController: InitControllerType = {
   //controller to install prometheus stack from help and update
   installPrometheus: async (req, res, next) => {
-    try{
-    console.log('prometheus initialization controller running')
-    spawnSync('helm repo add grafana https://grafana.github.io/helm-charts', { shell: true});
-    spawnSync('helm dependency build ./kube-prometheus-stack', {shell: true});
-    spawnSync('helm install prometheus ./kube-prometheus-stack', { shell: true });
-    return next();
+    try {
+      console.log('prometheus initialization controller running');
+      spawnSync('helm repo add grafana https://grafana.github.io/helm-charts', { shell: true });
+      spawnSync('helm dependency build ./kube-prometheus-stack', { shell: true });
+      spawnSync('helm install prometheus ./kube-prometheus-stack', { shell: true });
+      return next();
     } catch (err) {
       return next({
         log: `error in initController installPromteheus: ${err}`,
@@ -19,22 +19,27 @@ const initController: InitControllerType = {
     }
   },
 
-
   //controller to port forward Grafana so that the metrics page can start scrapping data
   installGrafana: (req, res, next) => {
-    try{
-    console.log('grafana initialization controller running')
-    // spawn('kubectl port-forward deployment/prometheus-grafana 3000', {shell: true, detached: true})
-    spawn('kubectl port-forward service/prometheus-grafana 9000:80', {shell: true, detached: true})
-    return next();
+    try {
+      console.log('grafana initialization controller running');
+      // spawn('kubectl port-forward deployment/prometheus-grafana 3000', {shell: true, detached: true})
+      const server = spawn('kubectl port-forward service/prometheus-grafana 9000:80', { shell: true, detached: true });
+      server.stdout.on('data', function (data) {
+        console.log('stdout: ' + data);
+      });
+      server.stderr.on('data', function (data) {
+        console.log('stderr: ' + data);
+      });
+      return next();
     } catch (err) {
-    return next({
-      log: `error in initController installGrafana: ${err}`,
-      status: 500,
-      message: { err: 'An error occurred port forwarding Grafana' },
+      return next({
+        log: `error in initController installGrafana: ${err}`,
+        status: 500,
+        message: { err: 'An error occurred port forwarding Grafana' },
       });
     }
-  }  
-}
+  },
+};
 
 export default initController;
