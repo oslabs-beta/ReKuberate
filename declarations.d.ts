33e30c1c88a03a8f02b28662d42e3f57b4c103d5
farrellkai <farrellkai@gmail.com>
2023-06-12 14:36:49 -0400

    bcrypt funcionality

--- declarations.d.ts
+++ declarations.d.ts
@@ -2,3 +2,4 @@ declare module '*.scss';
 declare module 'anychart-react';
 declare module 'anychart';
 declare module '*.png';
+declare module 'bcryptjs';
--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,6 +1,7 @@
 import db from '../models/dbModel.ts';
 import { NextFunction } from 'express-serve-static-core';
 import { ErrorHandler, userControllerType } from '../types';
+import bcrypt from 'bcryptjs';
 
 const userController: userControllerType = {
   //Checks if username exists in DB
@@ -33,9 +34,12 @@ const userController: userControllerType = {
       });
     }
     const { createUsername, createPassword } = req.body;
+    const hash: string = await bcrypt.hash(createPassword, 10);
+    console.log('createPassword is:', createPassword);
+    console.log('hashed password is:', hash);
     const sqlQuery: string = 'INSERT INTO people (username, password) VALUES ($1, $2)';
     try {
-      const data = await db.query(sqlQuery, [createUsername, createPassword]);
+      const data = await db.query(sqlQuery, [createUsername, hash]);
       res.locals.foundUser = { username: createUsername };
       return next();
     } catch (err) {
@@ -47,7 +51,6 @@ const userController: userControllerType = {
     }
   },
 
-  //verifies if password matches username
   checkPassword: async (req, res, next) => {
     console.log('checkPassword controller is running');
     //error to be thrown if username does not exist or password does not match username
@@ -59,19 +62,17 @@ const userController: userControllerType = {
 
     //check if user was found in previous middleware function
     if (!res.locals.foundUser) return next(error);
-    const { username } = res.locals.foundUser;
+    const { username, password } = res.locals.foundUser;
     const { createPassword } = req.body;
-
-    const sqlQuery: string = 'SELECT * FROM people WHERE username=$1 AND password=$2';
     try {
-      const data = await db.query(sqlQuery, [username, createPassword]);
-      if (!data.rows[0]) return next(error);
+      const compare: boolean = await bcrypt.compare(createPassword, password);
+      if (!compare) return next(error);
       else return next();
     } catch (err) {
       return next({
-        log: 'Error in userController.checkUser middleware function',
+        log: 'error in userController.checkPassword middleware function',
         status: 500,
-        message: { err: 'cannot check user' },
+        message: { err: 'cannot identify user' },
       });
     }
   },
