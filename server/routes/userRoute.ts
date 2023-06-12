5a27f9b06d6b7be9a44f356463c069c2ac4394c8
HShaw215 <hu.shaw215@gmail.com>
2023-06-12 11:47:13 -0400

    working on fixing front end com with backend

--- server/routes/userRoute.ts
+++ server/routes/userRoute.ts
@@ -5,12 +5,18 @@ import userController from '../controllers/userController.ts';
 import cookieController from '../controllers/cookieController.ts'
 import sessionController from '../controllers/sessionController.ts'
 
+//check for cookies associated with user
+userRoute.get('/', sessionController.hasCookie, (req, res) => {
+  res.status(200).send(res.locals.checked)
+})
+
 //create new user in DB
 userRoute.post('/signup', userController.checkUser, userController.createUser, cookieController.setSSIDCookie, (req, res) => {
   res.sendStatus(200);
 });
+
 //logins in user
-userRoute.post('/login', userController.checkUser, userController.checkPassword, cookieController.setSSIDCookie, (req, res) => { 
+userRoute.post('/login', (req, res, next) => {console.log('login here'); return next()}, userController.checkUser, userController.checkPassword, cookieController.setSSIDCookie, (req, res) => { 
   res.sendStatus(200)
 });
 
