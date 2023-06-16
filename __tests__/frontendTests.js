19facb3ab22967a3dca1fbd80b7c34a9312b8959
Fab3005 <fabrizzio7112@outlook.com>
2023-06-16 15:17:18 -0400

    added the boilerplate for the frontendTest

--- __tests__/frontendTests.js
+++ __tests__/frontendTests.js
@@ -1,4 +1,18 @@
 import puppeteer from 'puppeteer';
 import MouseHelper from '../server/controllers/mouseHelper.js';
 
+
 //login components testing
+
+(async () => {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: [`--window-size=1900,1000`],
+    })
+    //set puppeteer chrome window screensize for all users
+  await page.setViewport({ width: 1900, height: 1000 });
+  await installMouseHelper(page);
+
+  await page.goto('http://localhost:300');
+
+})();
