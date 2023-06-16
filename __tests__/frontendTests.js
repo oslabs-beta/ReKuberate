e4f3f81aa1501f5dc0c0a6c774e167481a2869f4
Fab3005 <fabrizzio7112@outlook.com>
2023-06-16 15:45:44 -0400

    added login form testing

--- __tests__/frontendTests.js
+++ __tests__/frontendTests.js
@@ -1,5 +1,6 @@
 import puppeteer from 'puppeteer';
 import MouseHelper from '../server/controllers/mouseHelper.js';
+import { sign } from 'crypto';
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -13,12 +14,19 @@ import MouseHelper from '../server/controllers/mouseHelper.js';
   await page.goto('http://localhost:300');
 
   //Main screen testing and all of it components in it for the login page
-  const mainScreen = '#root > div._91iyXizfLMZnut518R_X > div.UIOjVBEHqQKk3K6lPcQa'
-  await page.waitForSelector(mainScreen)
-  mainScreen ? console.log('Main Screen is present.') : console.log('Main screen is not present')
+  const mainScreen = '#root > div._91iyXizfLMZnut518R_X > div.UIOjVBEHqQKk3K6lPcQa';
+  await page.waitForSelector(mainScreen);
+  mainScreen ? console.log('Main Screen is present.') : console.log('Main screen is not present');
 
+  //Login Components
+  const loginComponents = '#root > div._91iyXizfLMZnut518R_X > div.UIOjVBEHqQKk3K6lPcQa > div';
+  await page.waitForSelector(loginComponents);
+  loginComponents ? console.log('the login components div is present') : console.log('the login components div is not present')
 
-
+  //Sign in Form
+  const signInForm = '#root > div._91iyXizfLMZnut518R_X > div.UIOjVBEHqQKk3K6lPcQa > div > form';
+  await page.waitForSelector(signInForm);
+  signInForm ? console.log('Sign in form is present') : console.log('Sign in form is not present')
 
   //login components testing
   const loginbutton = '#loginUsername';
