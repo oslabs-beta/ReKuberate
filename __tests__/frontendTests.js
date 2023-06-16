1ad28809ae08e5a84f95ebce09baf9d7d22aabeb
Fab3005 <fabrizzio7112@outlook.com>
2023-06-16 16:17:05 -0400

    changed page.gotto and added extra testing for the login component

--- __tests__/frontendTests.js
+++ __tests__/frontendTests.js
@@ -11,7 +11,7 @@ import MouseHelper from '../server/controllers/mouseHelper.js';
   await page.setViewport({ width: 1900, height: 1000 });
   await MouseHelper(page);
 
-  await page.goto('http://localhost:300');
+  await page.goto('http://localhost:8080');
 
   //Main screen testing and all of it components in it for the login page
   const mainScreen = '#root > div._91iyXizfLMZnut518R_X > div.UIOjVBEHqQKk3K6lPcQa';
@@ -28,9 +28,24 @@ import MouseHelper from '../server/controllers/mouseHelper.js';
   await page.waitForSelector(signInForm);
   signInForm ? console.log('Sign in form is present') : console.log('Sign in form is not present')
 
+  //Username label
+  const usernameLabel = '#root > div._91iyXizfLMZnut518R_X > div.UIOjVBEHqQKk3K6lPcQa > div > form > p:nth-child(1)';
+  await page.waitForSelector(usernameLabel);
+  usernameLabel ? console.log('username label is present') : console.log('username label is not present');
+
   //login components testing
-  const loginbutton = '#loginUsername';
-  await page.waitForSelector(loginbutton);
-  loginbutton ? console.log('Login button is present.') : console.log('Login button is not present')
+  const loginInput = '#loginUsername';
+  await page.waitForSelector(loginInput);
+  loginInput ? console.log('Login input is present.') : console.log('Login input is not present')
+
+  //password label
+  const passwordaLabel = '#root > div._91iyXizfLMZnut518R_X > div.UIOjVBEHqQKk3K6lPcQa > div > form > p:nth-child(3)';
+  await page.waitForSelector(passwordaLabel);
+  passwordInput ? console.log('password label is present') : console.log()
+
+  //password input
+  const passwordInput = '#loginPassword';
+  await page.waitForSelector(passwordInput);
+  passwordInput ? console.log('password input is present') : console.log('password input is not present');
 
 })();
