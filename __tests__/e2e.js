351ce09c75d3ec9a66553f18e68048ff2ab1af4f
farrellkai <farrellkai@gmail.com>
2023-06-17 09:34:59 -0400

    error if no username for new user

--- __tests__/e2e.js
+++ __tests__/e2e.js
@@ -64,12 +64,24 @@ describe('End to End Unit Tests', () => {
       expect(page.url()).toBe('http://localhost:8080/createAccount');
     });
 
-    it('Should throw an error if username is not provided', async () => {
+    xit('Should throw an error if username is not provided', async () => {
       await page.goto('http://localhost:8080/createAccount');
       await page.waitForSelector(username);
       await page.waitForSelector(password);
       await page.waitForSelector(loginButton);
       await page.type(password, 'testpassword');
+      await page.click(loginButton);
+      const errorMessage =
+        '#root > div._91iyXizfLMZnut518R_X > div.LnC6EgFrVh73ecrj0o0B > div > form > p.g5TRelzMwcZSywZtXjZF';
+      expect(errorMessage).toBeTruthy;
+    });
+
+    it('Should throw an error if password is not provided', async () => {
+      await page.goto('http://localhost:8080/createAccount');
+      await page.waitForSelector(username);
+      await page.waitForSelector(password);
+      await page.waitForSelector(loginButton);
+      await page.type(username, 'testusername');
       await page.click(loginButton);
       const errorMessage =
         '#root > div._91iyXizfLMZnut518R_X > div.LnC6EgFrVh73ecrj0o0B > div > form > p.g5TRelzMwcZSywZtXjZF';
