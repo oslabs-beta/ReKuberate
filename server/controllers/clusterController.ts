c19f55e60eab67cd2f11aa1e90246dced9781a21
thadd225 <thadd225@gmail.com>
2023-06-17 12:04:42 -0400

    Added comments to clusterController and removed chromium pop up from webScrapper

--- server/controllers/clusterController.ts
+++ server/controllers/clusterController.ts
@@ -4,21 +4,24 @@ import { ClusterControllerType, obj } from '../types';
 const clusterController: ClusterControllerType = {
   getPodAndNodeInfo: async (req, res, next) => {
     try {
+      //execute terminal command to get pod info
       const pods = spawnSync('kubectl get pod -o wide', {
         shell: true,
         encoding: 'utf-8',
       });
 
       const podsOutput = pods.stdout;
-      //way around using any??
-      const podsSplit: any = podsOutput.split(/[\n]/);
 
+      //remove all line breaks in terminal response
+      const podsSplit: any = podsOutput.split(/[\n]/);
+      //execute terminal command to get node status and save result to nodesOutput
       const nodes = spawnSync('minikube status', {
         shell: true,
         encoding: 'utf-8',
       });
-      const nodesOutput = nodes.output;
 
+      const nodesOutput = nodes.output;
+      //assign minikube to first index of nodesOutput and remove all linebreaks
       const minikube = nodesOutput[1].split(/[\n]/);
       const obj: obj = {};
       let currentContainer: string = minikube[0];
@@ -40,16 +43,16 @@ const clusterController: ClusterControllerType = {
       }
 
       for (let i = 1; i < minikube.length - 2; i++) {
+        //if current element is an actual node, set it as a key in obj, containing a pods key with a value of an array
         if (minikube[i] === '') {
           i++;
           currentContainer = minikube[i];
           obj[currentContainer] = {};
           i++;
           obj[currentContainer].pods = [];
-          //if string includes container name, push to container.pods
+          //if string in podsSplit includes container name, push to container.pods with name and status key value pairs
           for (let j = 1; j < podsSplit.length - 1; j++) {
             if (podsSplit[j].includes(currentContainer)) {
-              //work around any typing here?
               obj[currentContainer].pods.push({
                 name: podsSplit[j][0],
                 status: podsSplit[j][2],
@@ -58,7 +61,9 @@ const clusterController: ClusterControllerType = {
           }
         }
 
+        //create and separate each key, which is name of node property, by colon
         const key = minikube[i].slice(0, minikube[i].indexOf(': '));
+        //assign key's value to node's corresponding value from terminal output
         obj[currentContainer][key] = minikube[i].slice(minikube[i].indexOf(': ') + 2);
       }
       
