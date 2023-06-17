ad4bd1bf634fa126009d29f122331b82c68bf333
Kevin Fan <kevinlifan@yahoo.com>
2023-06-17 10:14:33 -0400

    added 8. in installation

--- frontend/containers/docsContainers/Installation.tsx
+++ frontend/containers/docsContainers/Installation.tsx
@@ -13,6 +13,10 @@ export default function Installation() {
         <p>5. Install dependencies with 'npm install'</p>
         <p>6. Start the application with 'npm run electron'</p>
         <p>7. Enjoy!</p>
+        <p>
+          8. If the metrics do not load but the boxes appear, try going to http://localhost:9000 and logging in with
+          **username:** admin, **password:** prom-operator
+        </p>
       </div>
     </>
   );
