1a91fb87ae09bb19128fc4039190eed8efa2c2ca
Fab3005 <fabrizzio7112@outlook.com>
2023-06-20 11:46:36 -0400

    init route psuedo

--- server/routes/initRoute.ts
+++ server/routes/initRoute.ts
@@ -3,6 +3,7 @@ import initController from '../controllers/initController.ts';
 
 const initRoute = express.Router();
 
+//route hander that goes through the prometheus, grafana stack middleware to obtain the metrics graphs
 initRoute.get('/', initController.installPrometheus, initController.installGrafana, (req: Request, res: Response, next: NextFunction) => {
     return res.status(200).json(res.locals.graphs);
   }
