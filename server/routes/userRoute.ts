61151a49a166fef02ec14ca5e293388faa6506a9
HShaw215 <hu.shaw215@gmail.com>
2023-06-20 11:31:09 -0400

    updated psuedocode for userRoute

--- server/routes/userRoute.ts
+++ server/routes/userRoute.ts
@@ -5,24 +5,24 @@ import userController from '../controllers/userController.ts';
 import cookieController from '../controllers/cookieController.ts';
 import sessionController from '../controllers/sessionController.ts';
 
-//checks for cookies associated with user and sends back boolean for frontend routing
+//Session Route: checks for cookies associated with user and sends back boolean for frontend routing
 userRoute.get('/', sessionController.hasCookie, (req, res) => {
   res.status(200).send(res.locals.checked);
 });
 
-//creates a new user in DB, checking for username availability and creates a session cookie
+//Signup Route: creates a new user in DB, checking for username availability and creates a session cookie
 userRoute.post('/signup', userController.checkUser, userController.createUser, cookieController.setSSIDCookie, (req, res) => {
     res.sendStatus(200);
   }
 );
 
-//logins user, checking for username in database, matching password, and creates a session cookie
+//Login Route: checks for username in database, matching password, and creates a session cookie
 userRoute.post('/login', userController.checkUser, userController.checkPassword, cookieController.setSSIDCookie, (req, res) => {
     res.sendStatus(200);
   }
 );
 
-//clears cookie when user logs out and reroutes to login page
+//Logout Route: clears cookie when user logs out and reroutes to login page
 userRoute.get('/logout', (req, res) => {
   return res.clearCookie('ssid').redirect('/');
 });
