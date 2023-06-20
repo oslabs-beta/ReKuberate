c03470498f757fa1a1cdc23de82c73ea34da7d68
thadd225 <thadd225@gmail.com>
2023-06-20 10:42:17 -0400

    removed unnecessary code and added more descriptive pseudocode in loginContainer

--- frontend/containers/LoginContainer.tsx
+++ frontend/containers/LoginContainer.tsx
@@ -14,6 +14,9 @@ export default function LoginContainer() {
     dispatch(setErrorMessage([]));
   }, []);
 
+  //send user's login input to backend to verify their account
+  //upon successful login, update loggedIn state to true and navigate to homepage
+  //if there is an error, display it and update errorMessage state
   const verifyLogin = async () => {
     const username = (document.getElementById('loginUsername') as HTMLInputElement).value;
     const password = (document.getElementById('loginPassword') as HTMLInputElement).value;
@@ -43,9 +46,8 @@ export default function LoginContainer() {
     }
   };
 
+  //initiate OAuth by sending user to github authorization page with app's client id
   const verifyGithub = () => {
-    //initiate OAuth
-    console.log('test github');
     const CLIENT_ID = '4661c408155c78af4f09';
     window.location.assign('https://github.com/login/oauth/authorize?client_id=' + CLIENT_ID);
   };
