caedf5710a10909d6279793387b6ee7544502c12
HShaw215 <hu.shaw215@gmail.com>
2023-06-21 11:41:42 -0400

    fixed typo in Installation page

--- frontend/containers/docsContainers/Installation.tsx
+++ frontend/containers/docsContainers/Installation.tsx
@@ -5,7 +5,7 @@ export default function Installation() {
   return (
     <>
       <div className={styles.docs} id="installation">
-        <p style={{ fontWeight: 600 }}>Installation is incredible simple</p>
+        <p style={{ fontWeight: 900 }}>Installation is incredibly simple</p>
         <p>1. Head over to ReKuberate.org</p>
         <p>2. Click on download</p>
         <p>3. Extract the zip file</p>
