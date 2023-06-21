41aad747876b46bb29ccd45b1c8ea1e436f05616
HShaw215 <hu.shaw215@gmail.com>
2023-06-21 10:47:43 -0400

    cleaned up about page

--- frontend/containers/docsContainers/About.tsx
+++ frontend/containers/docsContainers/About.tsx
@@ -9,12 +9,16 @@ export default function About() {
       <div className={styles.docs} id="about">
         <img src={gif1} className={styles.gifs} />
         <p>
-          First time setup of ReKuberate will require an initial log in. Log in data is hashed and authenticated with
-          bCrypt. Select the file location of where the application is downloaded and initiate data scraping from your
-          kubernetes cluster. Make sure your cluster is running or else no data will be collected.
+          First time setup of ReKuberate will require an initial sign up and login, where all info is hashed and authenticated.
+          Once at the home page, select submit to initialize data scraping from your kubernetes cluster. 
+          Make sure the cluster is running or else no data will be collected and a blank page will appear. Inital use will also require
+          visiting localhost:9000 to sign in to Grafana using admin and prom-operator as the username and password.
         </p>
         <img src={gif2} className={styles.gifs} />
-        <p>The nodes will be color-coded: red: error, yellow: pending, green: running.</p>
+        <p>The displayed nodes are color-coded:  
+          <span className={styles.red}>error</span>,  
+          <span className={styles.yellow}>pending</span>,  
+          <span className={styles.green}>running</span>.</p>
         <p>Hovering over each node will display additional details.</p>
       </div>
     </>
