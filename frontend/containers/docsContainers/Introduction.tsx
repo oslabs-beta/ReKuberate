6e45302d5d6eb381b3111c55c3be61194fd7482a
HShaw215 <hu.shaw215@gmail.com>
2023-06-21 10:58:02 -0400

    updated introduction wording

--- frontend/containers/docsContainers/Introduction.tsx
+++ frontend/containers/docsContainers/Introduction.tsx
@@ -6,11 +6,11 @@ export default function Introduction() {
     <>
       <div className={styles.docs} id="introduction">
         <p>
-          ReKuberate is a GUI that visualizes your kubernetes clusters in real time, allowing you to quickly see and
-          diagnose problems and exactly where they're located. The hierarchy of the cluster is displayed
-          straightforward, with each node and pod color-coordinated, and with additional information on hover. Curated
-          metrics on pod status and cluster health is also available. Compiled with electron and available for both
-          windows and macOS, downloading, installing, and initializing ReKuberate is quick and simple.
+          ReKuberate is a GUI that visualizes your kubernetes clusters in real time, allowing you to quickly
+          diagnose problems and view exactly where they're located. The hierarchy of the cluster is displayed
+          in a web, with each node and pod being color-coded. Additional information can be acquired by simply hovering over
+          each node. Curated metrics on pod status and cluster health is also available. Compiled with electron, ReKuberate is 
+          available for both windows and macOS, making downloading, installing, and initializing ReKuberate quick and simple.
         </p>
       </div>
     </>
