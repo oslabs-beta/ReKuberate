1667265436164060c77d889906615d6fce250c5f
HShaw215 <hu.shaw215@gmail.com>
2023-06-22 09:22:49 -0400

    updated for Helm instructions

--- frontend/containers/docsContainers/Installation.tsx
+++ frontend/containers/docsContainers/Installation.tsx
@@ -8,12 +8,13 @@ export default function Installation() {
         <p>1. Head over to ReKuberate.org</p>
         <p>2. Click on download</p>
         <p>3. Extract the zip file</p>
-        <p>4. Open the extracted folder in VSCode</p>
-        <p>5. Install dependencies with 'npm install'</p>
-        <p>6. Start the application with 'npm run electron'</p>
-        <p>7. If this is your first time using ReKuberate, visit <a href="http://localhost:9000">http://localhost:9000</a> and login 
+        <p>4. Make sure Helm is installed locally</p>
+        <p>5. Open the extracted folder in VSCode</p>
+        <p>6. Install dependencies with 'npm install'</p>
+        <p>7. Start the application with 'npm run electron'</p>
+        <p>8. If this is your first time using ReKuberate, visit <a href="http://localhost:9000">http://localhost:9000</a> and login 
         using admin and prom-operator as the username and password</p>
-        <p>8. Enjoy!</p>
+        <p>9. Enjoy!</p>
       </div>
     </>
   );
